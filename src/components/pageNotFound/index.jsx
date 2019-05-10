adaf50b70298d8b64d15bc751b3de4033ad3dfca
JpabloMF <juanpablo45671@gmail.com>
2019-05-10 13:31:41 -0500

    Initial commit

---
+++ src/components/pageNotFound/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PageNotFound = (props) => {
+  return <div>pageNotFound</div>;
+}
+
+export default PageNotFound;
---
+++ src/platform/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/* components */
+import Modal from '../shared/modal';
+import Button from "@material-ui/core/Button";
+
+const Platform = (props) => {
+  return(
+  	<div>Platform</div>
+  );
+}
+
+export default Platform;
