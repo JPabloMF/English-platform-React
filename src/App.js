b5d340db79de78f51831e55b879d7bd485eddd7f
JpabloMF <juanpablo45671@gmail.com>
2019-05-25 21:37:33 -0500

    Route for signin modified

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import "./App.css";
 import Dashboard from "./dashboard";
 import Platform from "./platform";
 import PageNotFound from "./components/pageNotFound";
+import SignIn from "./signin/";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Platform} />
+        <Route exact path="/" component={SignIn} />
+        <Route exact path="/Login" component={SignIn} />
         <Route path="/Dashboard" component={Dashboard} />
         <Route path="/Platform" component={Platform} />
         <Route component={PageNotFound} />
