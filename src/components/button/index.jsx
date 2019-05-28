ff39b534cdc859e15a43925bf616cf4b1f6047de
JpabloMF <juanpablo45671@gmail.com>
2019-05-27 23:18:15 -0500

    Adding styles to button component

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+
+// styles
+const StyledButton = withStyles({
+  root: {
+    background: (props) =>
+      props.bgcolor === "gradientblue"
+        ? "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)"
+        : "#fff",
+    color: (props) => (props.color ? props.color : "#000"),
+    margin: (props) => (props.margin ? props.margin : "0"),
+    padding: (props) => (props.padding ? props.padding : "0")
+  }
+})(Button);
 
 const ButtonComponent = ({
   text,
@@ -8,37 +22,49 @@ const ButtonComponent = ({
   type,
   variant,
   color,
+  bgcolor,
+  margin,
+  padding,
   disabled,
   onClick
 }) => {
   return (
-    <Button
+    <StyledButton
       name={name}
       type={type}
       variant={variant}
       color={color}
-      onClick={() => onClick ? onClick() : null}
+      bgcolor={bgcolor}
+      margin={margin}
+      padding={padding}
+      onClick={() => (onClick ? onClick() : null)}
       disabled={disabled}
     >
       {text}
-    </Button>
+    </StyledButton>
   );
-}
+};
 
 ButtonComponent.defaultProps = {
   disabled: false,
   type: "button",
   variant: "contained",
-  color: "primary"
+  color: "#000",
+  bgcolor: "#fff",
+  margin: "0",
+  padding: "5px"
 };
 
 ButtonComponent.prototypes = {
   text: PropTypes.string.isRequired,
   name: PropTypes.string,
   type: PropTypes.string,
-  variant: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-  disabled: PropTypes.bool.isRequired,
+  variant: PropTypes.string,
+  color: PropTypes.string,
+  bgcolor: PropTypes.string,
+  margin: PropTypes.string,
+  padding: PropTypes.string,
+  disabled: PropTypes.bool,
   onClick: PropTypes.func
 };
 
